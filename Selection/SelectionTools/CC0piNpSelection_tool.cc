1079c817a60702dc2ae6041d4bfc8e12fc8b0305
Stefano Roberto Soleti <stefano.soleti@physics.ox.ac.uk>
2019-04-11 17:06:40 -0500

    Some truth info

--- Selection/SelectionTools/CC0piNpSelection_tool.cc
+++ Selection/SelectionTools/CC0piNpSelection_tool.cc
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include "SelectionToolBase.h"
+#include "nusimdata/SimulationBase/MCTruth.h"
 
 namespace selection
 {
@@ -49,14 +50,23 @@ public:
     /**
      * @brief set branches for TTree
      */
-    void setBranches(TTree *_tree){};
+    void setBranches(TTree *_tree);
 
     /**
      * @brief reset ttree branches
      */
-    void resetTTree(TTree *_tree){};
+    void resetTTree(TTree *_tree);
 
 private:
+    /**
+     * @brief read truth info associated to pi0
+     */
+    void readTruth(art::Event const &e);
+
+    std::vector< double > _elec_vtx;
+    std::vector<double> _p_e;
+
+    std::vector < std::vector<double> > _p_vtx;
 };
 
 //----------------------------------------------------------------------------
@@ -89,10 +99,61 @@ void CC0piNpSelection::configure(fhicl::ParameterSet const &pset)
 /// pset - Fcl parameter set.
 ///
 bool CC0piNpSelection::selectEvent(art::Event const &e,
-                                 const std::vector<ProxyPfpElem_t> &pfp_pxy_v)
+                                   const std::vector<ProxyPfpElem_t> &pfp_pxy_v)
 {
 
+    if (!fData)
+        readTruth(e);
     return true;
+}
+
+void CC0piNpSelection::readTruth(art::Event const &e)
+{
+
+    std::cout << "CC0piNpSelection reading MCTruth" << std::endl;
+
+    auto const &mcparticles_handle = e.getValidHandle<std::vector<simb::MCParticle>>("largeant");
+    auto const &mcparticles(*mcparticles_handle);
+
+    for (auto &mcparticle : mcparticles) {
+        if (mcparticle.StatusCode() != 1)
+        {
+            continue;
+        }
+        if (mcparticle.PdgCode() == 13)
+        {
+            _elec_vtx.push_back(mcparticle.Trajectory().X(0));
+            _elec_vtx.push_back(mcparticle.Trajectory().Y(0));
+            _elec_vtx.push_back(mcparticle.Trajectory().Z(0));
+        }
+
+        if (mcparticle.PdgCode() == 2212)
+        {
+            std::vector< double > p_vertex;
+
+            p_vertex.push_back(mcparticle.Trajectory().X(0));
+            p_vertex.push_back(mcparticle.Trajectory().Y(0));
+            p_vertex.push_back(mcparticle.Trajectory().Z(0));
+
+            _p_vtx.push_back(p_vertex);
+            _p_e.push_back(mcparticle.E());
+        }
+    }
+}
+
+void CC0piNpSelection::resetTTree(TTree *_tree)
+{
+    std::cout << "CC0piNpSelection reset TTree" << std::endl;
+    _elec_vtx.clear();
+    _p_vtx.clear();
+    _p_e.clear();
+}
+
+void CC0piNpSelection::setBranches(TTree *_tree)
+{
+    _tree->Branch("elec_vtx", "std::vector< double >", &_elec_vtx);
+    _tree->Branch("p_vtx", "std::vector< std::vector< double > >", &_p_vtx);
+    _tree->Branch("p_e", "std::vector< double >", &_p_e);
 }
 
 DEFINE_ART_CLASS_TOOL(CC0piNpSelection)
