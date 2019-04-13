5f71f30c18d07d051eab3c62f7e97c7ef525cd33
David Caratelli <devish.c@gmail.com>
2019-04-13 17:14:25 -0500

    adding TrackPID skeleton which grabs Calo and PID info for a track. Needs to be filled --DC

--- Selection/CommonDefs/Typedefs.h
+++ Selection/CommonDefs/Typedefs.h
@@ -11,6 +11,9 @@
 #include "lardataobj/RecoBase/Track.h"
 #include "lardataobj/RecoBase/Vertex.h"
 
+#include "lardataobj/AnalysisBase/Calorimetry.h"
+#include "lardataobj/AnalysisBase/ParticleID.h"
+
 #include "lardataobj/RecoBase/PFParticleMetadata.h"
 #include "lardata/RecoBaseProxy/ProxyBase.h"
 
@@ -26,10 +29,23 @@ namespace searchingfornues {
                                               proxy::withAssociated<recob::Shower>(std::declval<art::InputTag>())) );
   using ProxyPfpElem_t = ProxyPfpColl_t::element_proxy_t;
 
+  // proxy to connect cluster to hit
   using ProxyClusColl_t = decltype(proxy::getCollection<std::vector<recob::Cluster> >(
 										      std::declval<art::Event>(),std::declval<art::InputTag>(),
 										      proxy::withAssociated<recob::Hit>(std::declval<art::InputTag>()) ) );
   using ProxyClusElem_t = ProxyClusColl_t::element_proxy_t;
+
+  // proxy to connect track to calorimetry object
+  using ProxyCaloColl_t = decltype(proxy::getCollection<std::vector<recob::Track> >(
+										      std::declval<art::Event>(),std::declval<art::InputTag>(),
+										      proxy::withAssociated<anab::Calorimetry>(std::declval<art::InputTag>()) ) );
+  using ProxyCaloElem_t = ProxyCaloColl_t::element_proxy_t;
+
+  // proxy to connect track to PID object
+  using ProxyPIDColl_t = decltype(proxy::getCollection<std::vector<recob::Track> >(
+										   std::declval<art::Event>(),std::declval<art::InputTag>(),
+										   proxy::withAssociated<anab::ParticleID>(std::declval<art::InputTag>()) ) );
+  using ProxyPIDElem_t = ProxyPIDColl_t::element_proxy_t;
 
 }
 
