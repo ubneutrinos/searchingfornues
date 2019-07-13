ae37cd4b58105ee5449a70b2ade98f655de780c3
David Caratelli <devish.c@gmail.com>
2019-07-13 13:14:34 -0500

    adding _tool to selection tool file name --DC

--- Selection/SelectionTools/CCincSelection_tool.cc
+++ Selection/SelectionTools/CCincSelection_tool.cc
@@ -4,6 +4,9 @@
 #include <iostream>
 #include "SelectionToolBase.h"
 #include "../CommonDefs/Typedefs.h"
+#include "../CommonDefs/BacktrackingFuncs.h"
+#include "../CommonDefs/Geometry.h"
+#include "../CommonDefs/SCECorrections.h"
 
 namespace selection
 {
@@ -205,11 +208,11 @@ bool CCincSelection::isFiducial(const double x[3]) const
       0., geo->DetLength()};
 
   bool is_x =
-      x[0] > (bnd[0] + fFidvolXstart) && x[0] < (bnd[1] - fFidvolXend);
+      x[0] > (bnd[0] + m_fidvolXstart) && x[0] < (bnd[1] - m_fidvolXend);
   bool is_y =
-      x[1] > (bnd[2] + fFidvolYstart) && x[1] < (bnd[3] - fFidvolYend);
+      x[1] > (bnd[2] + m_fidvolYstart) && x[1] < (bnd[3] - m_fidvolYend);
   bool is_z =
-      x[2] > (bnd[4] + fFidvolZstart) && x[2] < (bnd[5] - fFidvolZend);
+      x[2] > (bnd[4] + m_fidvolZstart) && x[2] < (bnd[5] - m_fidvolZend);
 
   return is_x && is_y && is_z;
 }
