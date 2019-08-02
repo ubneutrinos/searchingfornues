98903024310a67738d289b6286ff518d5cc3151e
SearchingForNues <travis@travis-ci.org>
2019-08-02 10:29:02 +0000

    Deploy code docs to GitHub Pages Travis build: 97
    Commit: 087dd105e8c78c0e351a33f6fa858c2fbeaec396

--- html/search/classes_9.js
+++ html/search/classes_9.js
@@ -3,6 +3,5 @@ var searchData=
   ['pi0selection',['Pi0Selection',['../classselection_1_1Pi0Selection.html',1,'selection']]],
   ['pi0truthanalysis',['Pi0TruthAnalysis',['../classanalysis_1_1Pi0TruthAnalysis.html',1,'analysis']]],
   ['pmtnoise',['PMTNoise',['../classanalysis_1_1PMTNoise.html',1,'analysis']]],
-  ['protonhitpurity',['ProtonHitPurity',['../classProtonHitPurity.html',1,'']]],
-  ['protontruthstudies',['ProtonTruthStudies',['../classProtonTruthStudies.html',1,'']]]
+  ['protonhitpurity',['ProtonHitPurity',['../classProtonHitPurity.html',1,'']]]
 ];
