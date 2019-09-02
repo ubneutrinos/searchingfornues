77621b4515ef02b393eaac8fe80d87907618feb6
SearchingForNues <travis@travis-ci.org>
2019-09-02 08:49:46 +0000

    Deploy code docs to GitHub Pages Travis build: 102
    Commit: 2d9146db3d87ece4e15631d2e368e8742a9db585

--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,9 +1,9 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefgimnoprst~",
-  1: "abcdefmnopst",
-  2: "abcdefginoprst~",
-  3: "_efmps",
+  0: "_abcdefgimnoprstv~",
+  1: "abcdefmnopstv",
+  2: "abcdefginoprstv~",
+  3: "_fm",
   4: "c",
   5: "e"
 };
