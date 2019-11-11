fe07f51f1000c72bfe0af8d3b0dcaf935e89a37c
Wouter Van De Pontseele <wouter.vandepontseele@physics.ox.ac.uk>
2019-11-11 10:45:38 -0600

    further cleanup of xml files

--- xml/xml_run_cc0pinp/run3/run_jobs_cc0pinp.sh
+++ xml/xml_run_cc0pinp/run3/run_jobs_cc0pinp.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-project.py --xml beam_on_cc0pinp.xml --stage run3_beam_on_cc0pinp --clean
-project.py --xml beam_on_cc0pinp.xml --stage run3_beam_on_cc0pinp --submit
-project.py --xml beam_off_cc0pinp.xml --stage run3_beam_off_cc0pinp --clean
-project.py --xml beam_off_cc0pinp.xml --stage run3_beam_off_cc0pinp --submit
+# Data
+#project.py --xml beam_on_cc0pinp.xml --stage run3_beam_on_cc0pinp --clean
+#project.py --xml beam_on_cc0pinp.xml --stage run3_beam_on_cc0pinp --submit
+#project.py --xml beam_off_cc0pinp.xml --stage run3_beam_off_cc0pinp --clean
+#project.py --xml beam_off_cc0pinp.xml --stage run3_beam_off_cc0pinp --submit
+
+# Default Overlay Samples
 project.py --xml bnb_nu_cc0pinp.xml --stage run3_bnb_nu_cc0pinp --clean
 project.py --xml bnb_nu_cc0pinp.xml --stage run3_bnb_nu_cc0pinp --submit
 project.py --xml bnb_nue_cc0pinp.xml --stage run3_bnb_nue_cc0pinp --clean
@@ -11,6 +14,7 @@ project.py --xml bnb_nue_cc0pinp.xml --stage run3_bnb_nue_cc0pinp --submit
 project.py --xml bnb_dirt_cc0pinp.xml --stage run3_bnb_dirt_cc0pinp --clean
 project.py --xml bnb_dirt_cc0pinp.xml --stage run3_bnb_dirt_cc0pinp --submit
 
+# Additional samples
 project.py --xml bnb_elee_low_cc0pinp.xml --stage run3_bnb_elee_low_cc0pinp --clean
 project.py --xml bnb_elee_low_cc0pinp.xml --stage run3_bnb_elee_low_cc0pinp --submit
 project.py --xml bnb_elee_high_cc0pinp.xml --stage run3_bnb_elee_high_cc0pinp --clean
