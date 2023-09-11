e9220c182745c37c4ec1c3003dcb466b03745ebe
Phidet <jpd64@cam.ac.uk>
2023-09-11 06:28:32 -0500

    Added fcl files for NuWro

--- Selection/AnalysisTools/DefaultAnalysis_tool.cc
+++ Selection/AnalysisTools/DefaultAnalysis_tool.cc
@@ -128,6 +128,7 @@ private:
   float fFidvolZend;
 
   bool fMakeNuMINtuple;
+  bool fIgnoreMCFlux;
 
   const int k_nu_e_other = 1;
   const int k_nu_e_cc0pi0p = 10;
@@ -349,6 +350,7 @@ DefaultAnalysis::DefaultAnalysis(const fhicl::ParameterSet &p)
   fFidvolZend = p.get<double>("fidvolZend");
 
   fMakeNuMINtuple = p.get<bool>("makeNuMINtuple", false);
+  fIgnoreMCFlux = p.get<bool>("ignoreMCFlux", false);
   NuMIOpFilterProd = p.get<std::string>("NuMIOpFiltProcName","");
   NuMISWTrigProd   = p.get<std::string>("NuMISWTriggerProcName","" );
 }
@@ -1407,53 +1409,55 @@ void DefaultAnalysis::SaveTruth(art::Event const &e)
   auto const &mcp_h = e.getValidHandle<std::vector<simb::MCParticle>>(fMCPproducer);
 
   // load MCFlux
-  auto const& mcflux_h = e.getValidHandle<std::vector<simb::MCFlux>>(fMCFluxproducer);
+  if(!fIgnoreMCFlux)
+  {
+    auto const& mcflux_h = e.getValidHandle<std::vector<simb::MCFlux>>(fMCFluxproducer);
+    if (mcflux_h.isValid()){ 
+      // reference: http://www.hep.utexas.edu/~zarko/wwwgnumi/v19/
+      /*
+        Decay mode that produced neutrino:
+        
+        1  K0L -> nue pi- e+
+        2  K0L -> nuebar pi+ e-
+        3  K0L -> numu pi- mu+
+        4  K0L -> numubar pi+ mu-
+        5  K+  -> numu mu+
+        6  K+  -> nue pi0 e+
+        7  K+  -> numu pi0 mu+
+        8  K-  -> numubar mu-
+        9  K-  -> nuebar pi0 e-
+        10  K-  -> numubar pi0 mu-
+        11  mu+ -> numubar nue e+
+        12  mu- -> numu nuebar e-
+        13  pi+ -> numu mu+
+        14  pi- -> numubar mu-
+      */
+      auto flux = mcflux_h->at(0);
+      _nu_parent_pdg = flux.fptype;
+      _nu_hadron_pdg = flux.ftptype;
+      _nu_decay_mode = flux.fndecay;
+      
+      if(fMakeNuMINtuple){
+        _par_decay_vx = flux.fvx;
+        _par_decay_vy = flux.fvy;
+        _par_decay_vz = flux.fvz;  
+        _par_decay_px = flux.fpdpx;
+        _par_decay_py = flux.fpdpy;
+        _par_decay_pz = flux.fpdpz;  
+        
+        TVector3 Trans_Targ2Det_beam = {5502, 7259, 67270};
+        TVector3 Decay_pos = {flux.fvx, flux.fvy, flux.fvz};
+        // std::cout << "\033[0;31mKrish:" << flux.fvx << " " << flux.fvy << " " << flux.fvz << "\033[0m" << std::endl;
+        
+        TVector3 baseline_vec = Trans_Targ2Det_beam - Decay_pos;
+        _baseline  = baseline_vec.Mag()/100.0;
 
-  if (mcflux_h.isValid()){ 
-    // reference: http://www.hep.utexas.edu/~zarko/wwwgnumi/v19/
-    /*
-      Decay mode that produced neutrino:
+      }
       
-      1  K0L -> nue pi- e+
-      2  K0L -> nuebar pi+ e-
-      3  K0L -> numu pi- mu+
-      4  K0L -> numubar pi+ mu-
-      5  K+  -> numu mu+
-      6  K+  -> nue pi0 e+
-      7  K+  -> numu pi0 mu+
-      8  K-  -> numubar mu-
-      9  K-  -> nuebar pi0 e-
-      10  K-  -> numubar pi0 mu-
-      11  mu+ -> numubar nue e+
-      12  mu- -> numu nuebar e-
-      13  pi+ -> numu mu+
-      14  pi- -> numubar mu-
-    */
-    auto flux = mcflux_h->at(0);
-    _nu_parent_pdg = flux.fptype;
-    _nu_hadron_pdg = flux.ftptype;
-    _nu_decay_mode = flux.fndecay;
-    
-    if(fMakeNuMINtuple){
-      _par_decay_vx = flux.fvx;
-      _par_decay_vy = flux.fvy;
-      _par_decay_vz = flux.fvz;  
-      _par_decay_px = flux.fpdpx;
-      _par_decay_py = flux.fpdpy;
-      _par_decay_pz = flux.fpdpz;  
-      
-      TVector3 Trans_Targ2Det_beam = {5502, 7259, 67270};
-      TVector3 Decay_pos = {flux.fvx, flux.fvy, flux.fvz};
-      // std::cout << "\033[0;31mKrish:" << flux.fvx << " " << flux.fvy << " " << flux.fvz << "\033[0m" << std::endl;
-      
-      TVector3 baseline_vec = Trans_Targ2Det_beam - Decay_pos;
-      _baseline  = baseline_vec.Mag()/100.0;
-
-    }
-    
-    _nu_l = flux.fdk2gen + flux.fgen2vtx;
-    std::cout << "total Lenght = " << flux.fdk2gen << " + " << flux.fgen2vtx << " = " << _nu_l << std::endl;
-  }// if flux handle is valid
+      _nu_l = flux.fdk2gen + flux.fgen2vtx;
+      std::cout << "total Lenght = " << flux.fdk2gen << " + " << flux.fgen2vtx << " = " << _nu_l << std::endl;
+    }// if flux handle is valid
+  }// if ignore MCFlux is false
   
   auto mct = mct_h->at(0);
   auto neutrino = mct.GetNeutrino();
